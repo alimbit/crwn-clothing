2fdcc99009d5ce4fcf6b813aaa1f0b974b22f21e
unknown <alibbit@gmail.com>
2020-04-11 12:30:35 -0700

    Create react app

---
+++ src/home.comp.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './home.style.scss';
+const HomePage =()=>( 
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>
+                        Shop now
+                        </span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>women</h1>
+                        <span className='subtitle'>
+                        Shop now
+                        </span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>men</h1>
+                        <span className='subtitle'>
+                        Shop now
+                        </span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Jackets</h1>
+                        <span className='subtitle'>
+                        Shop now
+                        </span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>sneakers</h1>
+                    <span className='subtitle'>
+                    Shop now
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
